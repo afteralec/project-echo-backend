3316f67ff449a8b9248ab0c9e3eea6d5030fb8b3
Alec DuBois <after.alec@gmail.com>
2020-09-09 17:12:30 -0700

    Updates listen button

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,18 +12,18 @@ UserListener.destroy_all
 Echo.destroy_all
 EchoListener.destroy_all
 
-amelie = User.create(first_name: "Amelie", last_name: "Oller", email: "amelie.oller@flatironschool.com")
-hal = User.create(first_name: "Hal", last_name: "Dunn", email: "hal.dunn@flatironschool.com")
-alec = User.create(first_name: "Alec", last_name: "DuBois", email: "after.alec@gmail.com")
-zana = User.create(first_name: "Zana", last_name: "Hertica", email: "zhertica@gmail.com")
-brad = User.create(first_name: "Brad", last_name: "Beggs", email: "brad.beggs@gmail.com")
-akiko = User.create(first_name: "Akiko", last_name: "Green", email: "agreen17@mail.ccsf.edu")
-daniel = User.create(first_name: "Daniel", last_name: "Patnode", email: "dbpatnode@hotmail.com")
-danira = User.create(first_name: "Danira", last_name: "Cortez", email: "cortezd334@yahoo.com")
-nathan = User.create(first_name: "Nathan", last_name: "Chung", email: "chung.nathanh@gmail.com")
-ronak = User.create(first_name: "Ronak", last_name: "Soni", email: "ronakvsoni@gmail.com")
-brandon = User.create(first_name: "Brandon", last_name: "Backer", email: "btbacker@gmail.com")
-don = User.create(first_name: "Don", last_name: "Mallory", email: "dmm333@gmail.com")
+amelie = User.create(first_name: "Amelie", last_name: "Oller", email: "amelie.oller@flatironschool.com", status: "Available")
+hal = User.create(first_name: "Hal", last_name: "Dunn", email: "hal.dunn@flatironschool.com", status: "Out Riding")
+alec = User.create(first_name: "Alec", last_name: "DuBois", email: "after.alec@gmail.com", status: "Probably Coding")
+zana = User.create(first_name: "Zana", last_name: "Hertica", email: "zhertica@gmail.com", status: "Practicing Backflips")
+brad = User.create(first_name: "Brad", last_name: "Beggs", email: "brad.beggs@gmail.com", status: "Climbing")
+akiko = User.create(first_name: "Akiko", last_name: "Green", email: "agreen17@mail.ccsf.edu", status: "Reachable")
+daniel = User.create(first_name: "Daniel", last_name: "Patnode", email: "dbpatnode@hotmail.com", status: "Hang Gliding")
+danira = User.create(first_name: "Danira", last_name: "Cortez", email: "cortezd334@yahoo.com", status: "Making Guac")
+nathan = User.create(first_name: "Nathan", last_name: "Chung", email: "chung.nathanh@gmail.com", status: "Making Pottery")
+ronak = User.create(first_name: "Ronak", last_name: "Soni", email: "ronakvsoni@gmail.com", status: "Working Out")
+brandon = User.create(first_name: "Brandon", last_name: "Backer", email: "btbacker@gmail.com", status: "Busy")
+don = User.create(first_name: "Don", last_name: "Mallory", email: "dmm333@gmail.com", status: "Away")
 
 amelie.listeners << hal
 amelie.listeners << alec
@@ -37,47 +37,47 @@ amelie.listeners << ronak
 amelie.listeners << brandon
 amelie.listeners << don
 
-hal.listeners << amelie
-alec.listeners << amelie
-zana.listeners << amelie
-brad.listeners << amelie
-akiko.listeners << amelie
-daniel.listeners << amelie
-danira.listeners << amelie
-nathan.listeners << amelie
-ronak.listeners << amelie
-brandon.listeners << amelie
-don.listeners << amelie
+# hal.listeners << amelie
+# alec.listeners << amelie
+# zana.listeners << amelie
+# brad.listeners << amelie
+# akiko.listeners << amelie
+# daniel.listeners << amelie
+# danira.listeners << amelie
+# nathan.listeners << amelie
+# ronak.listeners << amelie
+# brandon.listeners << amelie
+# don.listeners << amelie
 
-hal_echo = Echo.create(user: hal, message: "Amelie’s comprehensive knowledge of the course material is surpassed only by her natural talent in conveying it to the students")
-hal_echo.listeners << amelie
+# hal_echo = Echo.create(user: hal, message: "Amelie’s comprehensive knowledge of the course material is surpassed only by her natural talent in conveying it to the students")
+# hal_echo.listeners << amelie
 
-alec_echo = Echo.create(user: alec, message: "")
-alec_echo.listeners << amelie
+# alec_echo = Echo.create(user: alec, message: "")
+# alec_echo.listeners << amelie
 
-zana_echo = Echo.create(user: zana, message: "")
-zana_echo.listeners << amelie
+# zana_echo = Echo.create(user: zana, message: "")
+# zana_echo.listeners << amelie
 
-brad_echo = Echo.create(user: brad, message: "Amelie is able to clearly and simply explain the why, they how, the what, and the when.")
-brad_echo.listeners << amelie
+# brad_echo = Echo.create(user: brad, message: "Amelie is able to clearly and simply explain the why, they how, the what, and the when.")
+# brad_echo.listeners << amelie
 
-akiko_echo = Echo.create(user: akiko, message: "Amelie is a great instructor because she is thorough in her lectures. She has great patience and is always going above and beyond to make sure we as students are learning the material as best as we can")
-akiko_echo.listeners << amelie
+# akiko_echo = Echo.create(user: akiko, message: "Amelie is a great instructor because she is thorough in her lectures. She has great patience and is always going above and beyond to make sure we as students are learning the material as best as we can")
+# akiko_echo.listeners << amelie
 
-daniel_echo = Echo.create(user: daniel, message: "Amelie is always willing to lend a hand when needed, very informative and gives structured guidance without just giving the answer.")
-daniel_echo.listeners << amelie
+# daniel_echo = Echo.create(user: daniel, message: "Amelie is always willing to lend a hand when needed, very informative and gives structured guidance without just giving the answer.")
+# daniel_echo.listeners << amelie
 
-danira_echo = Echo.create(user: danira, message: "")
-danira_echo.listeners << amelie
+# danira_echo = Echo.create(user: danira, message: "")
+# danira_echo.listeners << amelie
 
-nathan_echo = Echo.create(user: nathan, message: "Amelie is a great instructor who cares about her students' success. It's so apparent that she knows her stuff and her love of Javascript is quite contagious. She's a bright and patient with us and continually guides us to do our best.")
-nathan_echo.listeners << amelie
+# nathan_echo = Echo.create(user: nathan, message: "Amelie is a great instructor who cares about her students' success. It's so apparent that she knows her stuff and her love of Javascript is quite contagious. She's a bright and patient with us and continually guides us to do our best.")
+# nathan_echo.listeners << amelie
 
-ronak_echo = Echo.create(user: ronak, message: "Amelie explains each concept of software engineering with in-depth knowledges. her way of approach to teaching students which is very appropriate for better understanding. Moreover, she is very polite nature and giving more ideas and techniques which helps students to enhancing their knowledge.")
-ronak_echo.listeners << amelie
+# ronak_echo = Echo.create(user: ronak, message: "Amelie explains each concept of software engineering with in-depth knowledges. her way of approach to teaching students which is very appropriate for better understanding. Moreover, she is very polite nature and giving more ideas and techniques which helps students to enhancing their knowledge.")
+# ronak_echo.listeners << amelie
 
-brandon_echo = Echo.create(user: brandon, message: "Amelie is always willing to spend a little extra time with a student answering questions and going over anything that may be unclear. She explains concepts from multiple angles and really seems to have a vested interest in student comprehension. I'm happy to have her as an instructor!")
-brandon_echo.listeners << amelie
+# brandon_echo = Echo.create(user: brandon, message: "Amelie is always willing to spend a little extra time with a student answering questions and going over anything that may be unclear. She explains concepts from multiple angles and really seems to have a vested interest in student comprehension. I'm happy to have her as an instructor!")
+# brandon_echo.listeners << amelie
 
-don_echo = Echo.create(user: don, message: "Amelie is willing to delve into rabbit holes")
-don_echo.listeners << amelie
+# don_echo = Echo.create(user: don, message: "Amelie is willing to delve into rabbit holes")
+# don_echo.listeners << amelie
